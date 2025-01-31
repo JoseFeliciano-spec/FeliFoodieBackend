c11a3f74c37590779bba3ce4b3b627b7846240bf
JoseFeliciano-spec <68482634+JoseFeliciano-spec@users.noreply.github.com>
2025-01-31 17:39:19 -0500

    fix: Arreglo de inicio

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
   app.enableCors();
 
   // Iniciar el servidor
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0'); 
 
   console.log(
     `La documentación de la API está disponible en: ${await app.getUrl()}/docs`,
