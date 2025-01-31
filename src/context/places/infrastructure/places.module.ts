1e4f29a88816008d91f3a540dbe5581f5be68c8e
JoseFeliciano-spec <68482634+JoseFeliciano-spec@users.noreply.github.com>
2025-01-31 18:01:05 -0500

    fix: Desactivar HistoryPlace Module

--- src/context/places/infrastructure/places.module.ts
+++ src/context/places/infrastructure/places.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PlacesController } from './http-api/v1/crud-places/crud-places.controller';
-import { HistoryPlaceSchema, HistoryPlace } from './schema/history.schema';
-import { FavoritePlaceSchema, FavoritePlace } from './schema/favorite.schema';
+/* import { HistoryPlaceSchema, HistoryPlace } from './schema/history.schema';
+ */import { FavoritePlaceSchema, FavoritePlace } from './schema/favorite.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { HistoryPlaceController } from './http-api/v1/historial-place/historial-place.controller';
-import { HistoryPlaceUseCases } from '../application/historial-place-use-case/historial-place.use-case';
+/* import { HistoryPlaceController } from './http-api/v1/historial-place/historial-place.controller';
+import { HistoryPlaceUseCases } from '../application/historial-place-use-case/historial-place.use-case'; */
 @Module({
   imports: [
-    MongooseModule.forFeature([
+    /* MongooseModule.forFeature([
       { name: HistoryPlace.name, schema: HistoryPlaceSchema },
-    ]),
+    ]), */
     MongooseModule.forFeature([
       { name: FavoritePlace.name, schema: FavoritePlaceSchema },
     ]),
   ],
-  controllers: [PlacesController, HistoryPlaceController],
-  providers: [HistoryPlaceUseCases],
+  controllers: [PlacesController, /* HistoryPlaceController */],
+  providers: [/* HistoryPlaceUseCases */],
   exports: [],
 })
 export class PlacesModules {}
